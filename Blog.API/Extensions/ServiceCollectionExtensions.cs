e722c79016194c6d5a1d89050dcecd95b053433e
jamol <jamol@LAPTOP-5TNOU2IH>
2023-07-06 10:04:34 +0500

    create jwt manager, add Service collection extention

---
+++ Blog.API/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Blog.API.Managers;
+using Blog.API.Repositories.Interfaces;
+using Blog.API.Repositories;
+
+namespace Blog.API.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddCustomServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddJwtConfiguration(configuration);
+            services.AddScoped<JwtTokenManager>();
+           
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IPostRepository,IPostRepository>();
+
+            services.AddScoped<UserProvider>();
+            services.AddHttpContextAccessor();
+        }
+
+        /* dockerda ishlatish uchun kere boladi
+        public static void MigrateIdentityDb(this WebApplication app)
+        {
+            if (app.Services.GetService<AppDbContext>() != null)
+            {
+                var identityDb = app.Services.GetRequiredService<AppDbContext>();
+                identityDb.Database.Migrate();
+            }
+        }*/
+
+    }
+}
---
+++ Blog.API/Providers/UserProvider.cs
@@ -0,0 +1,6 @@
+﻿namespace Blog.API.Providers
+{
+    public class UserProvider
+    {
+    }
+}
